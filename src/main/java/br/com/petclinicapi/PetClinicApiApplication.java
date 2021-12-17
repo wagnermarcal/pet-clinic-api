ebd7e7afd91714fee68ea5511071b0b85ee68c9e
Wagner Marçal <wagnermarcal.2008@gmail.com>
2021-12-17 10:49:32 -0300

    Creating Project

---
+++ src/main/java/br/com/petclinicapi/PetClinicApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.petclinicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/petclinicapi/PetClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petclinicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
